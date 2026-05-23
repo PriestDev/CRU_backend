191648f62b11025705b315fdc6ee155b4717c4a1
Braide Daniella Biobele <dannygodwin2003@gmail.com>
2026-05-23 15:10:36 +0100

    fix: corrected UI of the splash screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,30 @@
-'use client'
+"use client";
 
 import Image from "next/image";
 import Illustration from "../assets/undraw_order-ride_4gaq.svg";
-import { useEffect } from 'react'
-import { useRouter } from 'next/navigation'
+import { useRouter } from "next/navigation";
+import Button from "@/components/ui/button";
 
 export default function Home() {
-  const router = useRouter()
-  useEffect(() => {
-    const splashScreen = setTimeout(() => {router.replace('/signup')}, 2000)
-    return () => clearTimeout(splashScreen)
-  })
+  const router = useRouter();
+
   return (
-    <main className="flex flex-col flex-1 items-center justify-center bg-(--background) h-screen space-y-6">
+    <main className="flex flex-col flex-1 items-center justify-center bg-(--background) h-screen space-y-10 p-4">
       <Image
         src={Illustration}
         alt="Order ride illustration"
         width={400}
         height={500}
+        className="p-15 pb-0"
       />
-      <div>
-        <h1 className="text-2xl font-bold text-center text-(--primary)">
+      <div className="space-y-3">
+        <h1 className="text-3xl font-bold text-center text-(--primary)">
           Welcome to Campus Ride
         </h1>
         <p className="text-center">
           The most reliable way to navigate you campus effortlessly and safely.
         </p>
+        <Button text="Get Started" type="button" onClick={() => router.push("/signup")} bgColor="primary" />
       </div>
     </main>
   );
