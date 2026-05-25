c675ab05cab13995d1ee104b4abe86551ab5e117
Braide Daniella Biobele <dannygodwin2003@gmail.com>
2026-05-25 16:48:00 +0100

    Add: added the stylesheet link for my material symbols outlined icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${inter.variable} h-full antialiased`}>
+      <head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+      </head>
       <body className=" font-sans min-h-screen flex flex-col">
         <div className="hidden sm:flex min-h-screen items-center justify-center">Please view on mobile</div>
         <div className="sm:hidden">{children}</div>
