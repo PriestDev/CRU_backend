053e70b9cc5b0a3b2933d81e5e7a41f4e7a8bb0c
Braide Daniella Biobele <dannygodwin2003@gmail.com>
2026-05-25 20:55:50 +0100

    fix: fixed nav display in profile subpages

--- components/layout/nav.tsx
+++ components/layout/nav.tsx
@@ -7,37 +7,39 @@ const nav = () => {
   return (
     <div className="fixed bottom-0 right-0 left-0 m-4 bg-white/80 rounded-2xl shadow-xs border border-(--ash)/30 flex items-center justify-around p-4 gap-2 text-(--grey) text-sm backdrop-blur-sm">
       {/* home button */}
-      <Link href="/home"
+      <Link
+        href="/home"
         className={`flex flex-col items-center gap-1.5 ${pathname === "/home" && "text-(--primary) font-semibold"}`}
       >
-        {/* Added text-2xl here */}
         <span className="material-symbols-outlined text-2xl">home</span>
         <p className="">Home</p>
       </Link>
-      
+
       {/* activities button */}
-      <Link href="/activity"
+      <Link
+        href="/activity"
         className={`flex flex-col items-center gap-1.5 ${pathname === "/activity" && "text-(--primary) font-semibold"}`}
       >
-        {/* Added text-2xl here */}
         <span className="material-symbols-outlined text-2xl">history</span>
         <p className="">Activity</p>
       </Link>
-      
+
       {/* alerts button */}
-      <Link href="/alert"
+      <Link
+        href="/alert"
         className={`flex flex-col items-center gap-1.5 ${pathname === "/alert" && "text-(--primary) font-semibold"}`}
       >
-        {/* Added text-2xl here */}
-        <span className="material-symbols-outlined text-2xl">notifications</span>
+        <span className="material-symbols-outlined text-2xl">
+          notifications
+        </span>
         <p className="">Alert</p>
       </Link>
-      
+
       {/* profile button */}
-      <Link href="/profile"
-        className={`flex flex-col items-center gap-1.5 ${pathname === "/profile" && "text-(--primary) font-semibold"}`}
+      <Link
+        href="/profile"
+        className={`flex flex-col items-center gap-1.5 ${pathname === "/profile" || pathname === "/profile/personal-info" || pathname === "/profile/emergency-contacts" || pathname === "/profile/help" || pathname === "/profile/privacy-policy" ? "text-(--primary) font-semibold" : ""}`}
       >
-        {/* Added text-2xl here */}
         <span className="material-symbols-outlined text-2xl">person</span>
         <p className="">Profile</p>
       </Link>
