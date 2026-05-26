cbf1270e844d0245d83bacef561eb84e54c206cb
Braide Daniella Biobele <dannygodwin2003@gmail.com>
2026-05-26 09:46:50 +0100

    fix: fixed student layout.tsx to stop hydration error

--- app/(student)/layout.tsx
+++ app/(student)/layout.tsx
@@ -1,15 +1,13 @@
-import React, { ReactNode } from "react";
-import Nav from "@/components/layout/nav"
+import { ReactNode } from "react";
+import Nav from "@/components/layout/nav";
 
 const studentLayout = ({ children }: { children: ReactNode }) => {
-  return <html lang="en">
-    <body className="">
-        <div className="pb-30 bg-(--background) min-h-screen">
-          {children}
-        </div>
-        <Nav/>
-    </body>
-  </html>;
+  return (
+    <>
+      <div className="pb-30 bg-(--background) min-h-screen">{children}</div>
+      <Nav />
+    </>
+  );
 };
 
 export default studentLayout;
