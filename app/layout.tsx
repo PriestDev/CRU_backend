57d8ae0be9b9cd421fa3d3bf67c06f9b1d4884f4
Braide Daniella Biobele <dannygodwin2003@gmail.com>
2026-05-26 09:46:27 +0100

    fix: changed font of website

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Plus_Jakarta_Sans} from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({
-  variable: "--font-inter",
+const webFont = Plus_Jakarta_Sans({
+  variable: "--font-webFont",
   subsets: ["latin"],
 });
 
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${inter.variable} h-full antialiased`}>
+    <html lang="en" className={`${webFont.variable} h-full antialiased`}>
       <head>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
       </head>
