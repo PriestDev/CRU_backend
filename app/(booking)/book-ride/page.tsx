d99d95b0362d5255612019887a4d9f67e587a6c3
Braide Daniella Biobele <dannygodwin2003@gmail.com>
2026-05-28 22:22:17 +0100

    add: added UI's for booking pages.

--- app/(booking)/book-ride/page.tsx
+++ app/(booking)/book-ride/page.tsx
@@ -1,9 +1,60 @@
-import React from 'react'
+"use client";
+
+import PageHeader from "@/components/layout/pageHeader";
+import { useState } from "react";
+import dynamic from "next/dynamic";
+
+// const Map = dynamic(() => import("@/components/ui/map"), {
+//   ssr: false,
+// });
 
 const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+  const [tripType, setTripType] = useState<
+    "oneWay" | "roundTrip" | "multiStops" | "schedule"
+    >("oneWay");
+  const [activeSection, setActiveSection] = useState(0)
 
-export default page
+  return (
+    <div>
+      <div className="border-b border-(--stroke)">
+        <PageHeader text="Book A Ride" />
+      </div>
+      {/* <Map/> */}
+      <div className="h-[60vh]"></div>
+      <main className="bg-white rounded-t-4xl mt-[-20vh] z-100">
+        <div className="border-b border-(--stroke) px-4 text-(--lightText) grid grid-cols-4 text-center cursor-pointer">
+          {["One way", "Round trip", "Multi stop", "Schedule"].map(
+            (trip, index) => (
+              <div
+                className={`py-4 border-b-2 ${index === activeSection ? "border-(--primary) font-bold" : "border-transparent font-normal"}`}
+                key={index}
+                onClick={() => {
+                  setTripType(
+                    index === 0
+                      ? "oneWay"
+                      : index === 1
+                        ? "roundTrip"
+                        : index === 2
+                          ? "multiStops"
+                          : "schedule",
+                  );
+                  setActiveSection(index);
+                }}
+              >
+                {trip}
+              </div>
+            ),
+          )}
+        </div>
+        <form action="" className="p-4">
+          {tripType === "oneWay" && <div>One Way</div>}
+          {tripType === "roundTrip" && <div>Round Trip</div>}
+          {tripType === "multiStops" && <div>Multi Stops</div>}
+          {tripType === "schedule" && <div>Scedule</div>}
+        </form>
+      </main>
+    </div>
+  );
+};
+
+export default page;
--- app/(booking)/carpool/page.tsx
+++ app/(booking)/carpool/page.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+"use client";
+
+import PageHeader from "@/components/layout/pageHeader";
+import CarpoolCard from "@/components/ui/carpoolcard";
+import { carpoolTrips } from "../data";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div>
+      <div className="border-b border-(--stroke)">
+        <PageHeader text="Carpooling" />
+      </div>
+      <div className="p-4 space-y-2">
+        {carpoolTrips.map((carpool, index) => (
+          <div key={index}>
+            <CarpoolCard
+              id={carpool.id}
+              driverName={carpool.driverName}
+              driverImage={carpool.driverImage}
+              rating={carpool.rating}
+              totalRides={carpool.totalRides}
+              from={carpool.from}
+              to={carpool.to}
+              departureTime={carpool.departureTime}
+              arrivalTime={carpool.arrivalTime}
+              pricePerSeat={carpool.pricePerSeat}
+              seatsLeft={carpool.seatsLeft}
+              vehicleType={carpool.vehicleType}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default page;
