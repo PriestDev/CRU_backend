71f4da3d8504debf2dcd1d752234ce902d9f5243
Braide Daniella Biobele <dannygodwin2003@gmail.com>
2026-05-29 13:31:49 +0100

    add: emebedded google map in the book ride's page

--- app/(booking)/book-ride/page.tsx
+++ app/(booking)/book-ride/page.tsx
@@ -20,8 +20,10 @@ const page = () => {
         <PageHeader text="Book A Ride" />
       </div>
       {/* <Map/> */}
-      <div className="h-[60vh]"></div>
-      <main className="bg-white rounded-t-4xl mt-[-20vh] z-100">
+      <div className="h-[60vh]">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3975.254957898318!2d6.917814454244981!3d4.896932192097978!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1069da7f7bf8c433%3A0x6665c08e429606dd!2sUniversity%20of%20Port%20Harcourt!5e0!3m2!1sen!2sng!4v1780049037438!5m2!1sen!2sng" width="600" height="350" style={{border:0}} allowFullScreen={true} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+      <main className="bg-white rounded-t-4xl mt-[-20vh] z-60! min-h-[40vh]">
         <div className="border-b border-(--stroke) px-4 text-(--lightText) grid grid-cols-4 text-center cursor-pointer">
           {["One way", "Round trip", "Multi stop", "Schedule"].map(
             (trip, index) => (
